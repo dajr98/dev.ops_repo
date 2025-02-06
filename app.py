a1e7f2db5c9235f68e6f24dda4b6ad8df64edf23
dajr98 <136416633+dajr98@users.noreply.github.com>
2025-02-06 16:03:44 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# This is a band name generator based the users input
+print("Hello, welcome to the band naming generator! A simple yet cool way to come up with an original band name.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is your pets name?\n")
+print("Your potential band name could be", city + " " + pet)
