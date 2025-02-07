643f50ae4e0faf1bf8fc762f3e27e65821578205
dajr98 <136416633+dajr98@users.noreply.github.com>
2025-02-07 09:00:00 -0500

    Update name_gen_app.py

--- name_gen_app.py
+++ name_gen_app.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 
-st.write("Hello, welcome to the band naming generator! A simple yet cool way to come up with an original band name.")
+st.write("Hello, welcome to the funky naming generator! A simple yet cool way to come up with a fun and unique name.")
 city = st.text_input("Which city did you grow up in?\n")
 pet = st.text_input("What is your pets name?\n")
 color = st.text_input("What is your favorite color?\n")
