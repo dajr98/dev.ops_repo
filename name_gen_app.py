09b5a4f7bfdc74da1b31a8dac6bc0356ed86b852
dajr98 <136416633+dajr98@users.noreply.github.com>
2025-02-07 08:34:02 -0500

    Update name_gen_app.py

--- name_gen_app.py
+++ name_gen_app.py
@@ -1,9 +1,12 @@
 # This is a band name generator based the users input
 
-print("Hello, welcome to the band naming generator! A simple yet cool way to come up with an original band name.")
-city = input("Which city did you grow up in?\n")
-pet = input("What is your pets name?\n")
-color = input("What is your favorite color?\n")
-print("Your potential band name could be", city + " " + pet + " " + color)
+st.write()("Hello, welcome to the band naming generator! A simple yet cool way to come up with an original band name.")
+city = st.text_input("Which city did you grow up in?\n")
+pet = st.text_input("What is your pets name?\n")
+color = st.text_input("What is your favorite color?\n")
+st.write("Your potential band name could be", city + " " + pet + " " + color)
+
+
+
 
 
