93e7367dea426918a771726d84c1892f4062fbad
dajr98 <136416633+dajr98@users.noreply.github.com>
2025-02-20 19:44:13 -0500

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,6 +1,7 @@
-#Rock_Paper_Scissors
+import streamlit as st
+import random
 
-
+# Displaying the images for Rock, Paper, and Scissors
 rock = '''
     _______
 ---'   ____)
@@ -27,36 +28,28 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-import random
+
 game_images = [rock, paper, scissors]
 
-user_choice = int(input("Make your choice... Choose 0 for Rock. 1 for Paper. Or 2 for Scissors."))
+# Display the game title
+st.title("Rock Paper Scissors Game")
 
-if user_choice >= 0 and user_choice <= 2:
-    print(game_images[user_choice])
+# Let the user select their choice using radio buttons
+user_choice = st.radio("Make your choice... Choose 0 for Rock, 1 for Paper, or 2 for Scissors.", (0, 1, 2))
 
-computer_choice = random.randint(0,2)
-print("The computer chose:")
-print(game_images[computer_choice])
+# Display the image of the user's choice
+st.write("You chose:")
+st.image(game_images[user_choice], width=100)
 
-if user_choice >=3 or user_choice < 0:
-    print("That wasn't an option! You lose. :( ")
+# Generate the computer's choice
+computer_choice = random.randint(0, 2)
+st.write("The computer chose:")
+st.image(game_images[computer_choice], width=100)
 
-elif user_choice == 0 and computer_choice == 2:
-    print("You won!")
-
-elif user_choice == 2 and computer_choice == 0:
-    print("Sorry. You lost!")
-elif computer_choice > user_choice:
-    print("Sorry. You lost!")
-elif user_choice > computer_choice:
-    print("Nice move! You won!")
-elif computer_choice == user_choice:
-    print("It's a stalemate!")
-
-
-
-
-
-
-
+# Determine and display the result of the game
+if user_choice == computer_choice:
+    st.write("It's a stalemate!")
+elif (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+    st.write("You won!")
+else:
+    st.write("Sorry. You lost!")
